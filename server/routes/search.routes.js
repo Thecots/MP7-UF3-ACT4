70368972b44c5755068221fff19cb94b443847a8
Thecots <dcots@insdanielblanxart.cat>
2022-01-27 17:55:06 +0100

    v.0.2

--- server/routes/search.routes.js
+++ server/routes/search.routes.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const {checkUser} = require('./../middlewares/auth');
+const mysql = require('mysql');
+
+const getDataBase = () => {
+  return mysql.createConnection({
+    host: "localhost",
+    user: "daw_user",
+    password: "P@ssw0rd",
+    database: 'connect4'
+  });
+}
 
 router.get('/search',checkUser, (req, res) => {
-  console.log(req.id);
-  res.render("gameSearch");
+  let con = getDataBase();
+  con.connect(function (err) {
+    if (err) throw res.json({ ok: false });
+    res.render("gameSearch",{ok:false});
+  });
 })
 
 module.exports = router;
